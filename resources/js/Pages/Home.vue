9ba03dd922b7f4e23d3ea85f071a1181c54dfd6b
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-07-12 07:00:03 -0700

    Added carousel and posts section to Home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,115 @@
 <script setup>
+import { ref, onMounted, toRefs } from "vue";
+import { Head, Link, router } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
+
+// Carousel imports
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+
+let windowWidth = ref(window.innerWidth);
+let currentSlide = ref(0);
+let currentPost = ref(null);
+let openOverlay = ref(false);
+
+const props = defineProps({ posts: Object, allUsers: Object });
+const { posts, allUsers } = toRefs(props);
+
+onMounted(() => {
+    // Listen to the 'resize' event
+    window.addEventListener("resize", () => {
+        windowWidth.value = window.innerWidth;
+    });
+});
 </script>
 
 <template>
-    <Head title="Home" />
+    <Head title="Instagram" />
     <MainLayout>
-        <div class="bg-green-500">Home</div>
+        <div class="mx-auto lg:pl-0 md:pl-[80px] pl-0">
+            <Carousel
+                v-model="currentSlide"
+                class="max-w-[700px] mx-auto"
+                :items-to-show="windowWidth >= 768 ? 8 : 6"
+                :items-to-scroll="4"
+                :wrap-around="true"
+                :transition="500"
+                snapAlign="start"
+            >
+                <Slide v-for="slide in 10" :key="slide">
+                    <Link
+                        href="/"
+                        class="relative mx-auto text-center mt-4 px-2 cursor-pointer"
+                    >
+                        <div
+                            class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500"
+                        >
+                            <div
+                                class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white"
+                            />
+                        </div>
+                        <img
+                            class="rounded-full w-[56px] h-[56px] -mt-[1px]"
+                            src="https://i.pinimg.com/originals/8b/3e/55/8b3e5506a8f46851b336ef24224adcb0.jpg"
+                        />
+                        <div
+                            class="text-xs mt-2 w-[60px] truncate text-ellipsis overflow-hidden"
+                        >
+                            NAME
+                        </div>
+                    </Link>
+                </Slide>
+
+                <template #addons>
+                    <Navigation />
+                </template>
+            </Carousel>
+
+            <div id="Posts" class="px-4 max-w-[600px] mx-auto mt-10">
+                <div class="flex items-center justify-between py-2">
+                    <div class="flex items-center">
+                        <Link href="/" class="flex items-center">
+                            <img
+                                class="rounded-full w-[38px] h-[38px]"
+                                src="https://i.pinimg.com/originals/8b/3e/55/8b3e5506a8f46851b336ef24224adcb0.jpg"
+                            />
+                            <div class="ml-4 font-extrabold text[15px]">
+                                NAME
+                            </div>
+                        </Link>
+                        <div
+                            class="flex items-center text-[15px] text-gray-500"
+                        >
+                            <span class="-mt-5 ml-2 mr-[5px] text-[35px]"
+                                >.</span
+                            >
+                            <div>DATE</div>
+                        </div>
+                    </div>
+                    <DotsHorizontal class="cursor-pointer" :size="27" />
+                </div>
+
+                <div
+                    class="bg-black rounded-lg w-full min-h-[400px] flex items-center"
+                >
+                    <img
+                        class="mx-auto w-full"
+                        src="https://i.pinimg.com/originals/8b/3e/55/8b3e5506a8f46851b336ef24224adcb0.jpg"
+                    />
+                </div>
+            </div>
+        </div>
     </MainLayout>
 </template>
+
+<style>
+.carousel__prev,
+.carousel__next,
+.carousel__prev:hover,
+.carousel__next:hover {
+    color: rgb(49, 49, 49);
+    background-color: rgb(255, 255, 255);
+    border-radius: 100%;
+    margin: 0 20px;
+}
+</style>
